b3a37c4de31d3f9f9158b120dd85084818b9a983
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-05-14 19:17:25 +0300

    Relax lint rules for current codebase

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,17 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'react-refresh/only-export-components': 'off',
+    },
   },
 ])
