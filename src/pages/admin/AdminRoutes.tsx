3fd6df0c4423583227714f53cc204c33468e8708
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-06-10 10:56:45 +0300

    Show calendar preview inside admin dashboard

--- src/pages/admin/AdminRoutes.tsx
+++ src/pages/admin/AdminRoutes.tsx
@@ -13,28 +13,33 @@ import PortfolioCategoryEditAdmin from "./categoryPanel/PortfolioCategoryEditAdm
 import PortfolioCategoryAlbumsAdmin from "./categoryPanel/PortfolioCategoryAlbumsAdmin"
 import PrintRequestsAdmin from "./PrintRequestsAdmin"
 import CalendarAdmin from "./CalendarAdmin"
+import AdminDashboardCalendarPreview from "../../components/admin/AdminDashboardCalendarPreview"
 
 export default function AdminRoutes() {
     return (
-        <Routes>
-            <Route element={<AdminLayout />}>
-                <Route index element={<AdminPanel />} />
-                <Route path="users" element={<UsersAdmin />} />
-                <Route path="users/:id/albums" element={<UserAlbumsAdmin />} />
-                <Route path="contact-requests" element={<ContactRequestsAdmin />} />
-                <Route path="contact-requests/:id" element={<ContactRequestEditAdmin />} />
-                <Route path="contact-requests/:id/edit" element={<ContactRequestEditAdmin />} />
-                <Route path="print-requests" element={<PrintRequestsAdmin />} />
-                <Route path="calendar" element={<CalendarAdmin />} />
+        <>
+            <AdminDashboardCalendarPreview />
 
-                <Route path="client-galleries/new" element={<ClientGalleryEditAdmin />} />
-                <Route path="client-galleries/edit" element={<ClientGalleryEditAdmin />} />
-                <Route path="client-galleries/access" element={<ClientGalleryAccessAdmin />} />
+            <Routes>
+                <Route element={<AdminLayout />}>
+                    <Route index element={<AdminPanel />} />
+                    <Route path="users" element={<UsersAdmin />} />
+                    <Route path="users/:id/albums" element={<UserAlbumsAdmin />} />
+                    <Route path="contact-requests" element={<ContactRequestsAdmin />} />
+                    <Route path="contact-requests/:id" element={<ContactRequestEditAdmin />} />
+                    <Route path="contact-requests/:id/edit" element={<ContactRequestEditAdmin />} />
+                    <Route path="print-requests" element={<PrintRequestsAdmin />} />
+                    <Route path="calendar" element={<CalendarAdmin />} />
 
-                <Route path="portfolio-categories/new" element={<PortfolioCategoryCreateAdmin />} />
-                <Route path="portfolio-categories/edit" element={<PortfolioCategoryEditAdmin />} />
-                <Route path="portfolio-categories/albums" element={<PortfolioCategoryAlbumsAdmin />} />
-            </Route>
-        </Routes>
+                    <Route path="client-galleries/new" element={<ClientGalleryEditAdmin />} />
+                    <Route path="client-galleries/edit" element={<ClientGalleryEditAdmin />} />
+                    <Route path="client-galleries/access" element={<ClientGalleryAccessAdmin />} />
+
+                    <Route path="portfolio-categories/new" element={<PortfolioCategoryCreateAdmin />} />
+                    <Route path="portfolio-categories/edit" element={<PortfolioCategoryEditAdmin />} />
+                    <Route path="portfolio-categories/albums" element={<PortfolioCategoryAlbumsAdmin />} />
+                </Route>
+            </Routes>
+        </>
     )
 }
