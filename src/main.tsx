1c83e3d18b5ed4114999d6b1519b0f6c2a97b3ba
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-07-17 03:29:26 +0300

    Enable full album navigation from home slideshow

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import "./styles/homeSlideshowControls.css"
 import "./styles/homeServicesGrid.css"
 import "./i18n/i18n"
 import App from "./App"
+import HomeSlideshowAlbumLightbox from "./components/HomeSlideshowAlbumLightbox"
 import { AuthProvider } from "./context/AuthContext"
 
 const pendingRecentAlbumKey = "dgvisionstudio:pending-recent-album"
@@ -130,6 +131,7 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <AuthProvider>
           <App />
+          <HomeSlideshowAlbumLightbox />
         </AuthProvider>
       </BrowserRouter>
     </HelmetProvider>
