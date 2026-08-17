65e8d3eff1670d5cce1d0ceedbd38f7204f672a7
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-17 13:17:15 +0300

    Apply euro currency guard globally

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "./styles/foldHomeLayout.css"
 import "./styles/adminResponsive.css"
 import "./i18n/i18n"
 import App from "./App"
+import EuroCurrencyGuard from "./components/EuroCurrencyGuard"
 import HomeSlideshowAlbumLightbox from "./components/HomeSlideshowAlbumLightbox"
 import HomeFoldRecentAlbums from "./components/HomeFoldRecentAlbums"
 import MobileAdminCalendar from "./components/admin/MobileAdminCalendar"
@@ -135,6 +136,7 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <AuthProvider>
           <App />
+          <EuroCurrencyGuard />
           <MobileAdminCalendar />
           <HomeSlideshowAlbumLightbox />
           <HomeFoldRecentAlbums />
